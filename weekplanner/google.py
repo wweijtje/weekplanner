ad7b17f8726ab7150b71d0410dbaee6293bb1ef9
wweijtje <85878088+wweijtje@users.noreply.github.com>
2025-12-20 20:08:35 +0100

    Bugfix: missing import in google.py

--- weekplanner/google.py
+++ weekplanner/google.py
@@ -6,6 +6,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from pytz import utc
 
 
 def get_timestamp_from_google(date_dict):
