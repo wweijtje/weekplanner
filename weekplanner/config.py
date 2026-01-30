376d87d4a32b3fee446bd6f29a4608d7ac322da4
wweijtje <85878088+wweijtje@users.noreply.github.com>
2026-01-30 17:42:52 +0100

    Simplified way of handling events

--- weekplanner/config.py
+++ weekplanner/config.py
@@ -1,18 +1,11 @@
-def all_event_keywords(config, category="events"):
-    all_keywords = ''
+def find_event(event_name, config, category="events"):
+    # Normalize the input once
+    search_term = event_name.lower().strip()
     for _e in config[category]:
         _c = config[category][_e]
-        all_keywords += ''.join(_c['keywords'])
-    all_keywords = all_keywords.lower().replace(' ','')
-    return all_keywords
 
-
-def find_event(event_name, config, category="events"):
-
-    if event_name.lower().replace(' ','') in all_event_keywords(config=config, category=category):
-        for _e in config[category]:
-            _c = config[category][_e]
-            if event_name.lower().replace(' ','') in ''.join(_c['keywords']).lower().replace(' ',''):
+        for _k in _c['keywords']:
+            if _k.lower() in search_term:
                 return _c, _e
-    else:
-        return None
+    # Fall back, return None
+    return None
